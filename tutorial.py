ffcdd64d37aab06e880da60245effbcb09750fe0
GeorgeSkaria <georgeskaria2003@gmail.com>
2024-07-12 21:47:31 +0530

    new pipe class started

--- tutorial.py
+++ tutorial.py
@@ -85,6 +85,34 @@ Descending: If the bird is descending (d >= 0 and its y position is significantl
 
   def get_mask(self):
     return pygame.mask.from_surface(self.img)
+  
+
+class Pipe:
+  GAP=200
+  VEL=5
+
+  def __init__(self,x):
+    self.x=x
+    self.height=0
+    self.top=0
+    self.bottom=0
+    self.PIPE_TOP=pygame.transform.flip(PIPE_IMG,False,True)
+    self.PIPE_BOTTOM=PIPE_IMG
+    
+    self.passed=False
+    self.set_height()
+
+  def set_height(self):
+    self.height=random.randrange(50,450)
+    self.top=self.height-self.PIPE_TOP.get_height()
+    self.bottom=self.height+self.GAP
+
+  def move(self):
+    self.x-=self.VEL
+
+  
+
+
        
 def draw_window(win,bird):
   win.blit(BG_IMG,(0,0))
